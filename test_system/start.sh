4f7f18d445f1dcad84043ebff74bdf4aa2ee2407
Teemu Autto <teemu@rautakuu.org>
2024-06-16 22:26:02 +0300

    Restructured docker compose setup

--- test_system/start.sh
+++ test_system/start.sh
@@ -1,2 +1,14 @@
-cp orchestrator-init wasmiot-orchestrator/init
-docker compose --env-file .env -f docker-compose.example-system.yml --profile device up --build
+#!/bin/bash
+# Get this file directory
+DIR=$(dirname "${BASH_SOURCE[0]}")
+
+# Check for .env file
+if [ ! -f "${DIR}/.env" ]; then
+    echo "Creating .env file"
+    cp "${DIR}/.env.example" "${DIR}/.env"
+fi
+
+# Implement the docker-compose.yml file
+#cp "${DIR}/orchestrator-init "${DIR}/wasmiot-orchestrator/init"
+
+docker compose -f "${DIR}/docker-compose.yml" --profile device up --build --pull always
