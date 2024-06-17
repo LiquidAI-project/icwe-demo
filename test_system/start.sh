beb61259139e8b7ed6cd395ecbe3a6cc2d7e67f3
Teemu Autto <teemu@rautakuu.org>
2024-06-17 03:05:13 +0000

    Init git submodules if necessary.

--- test_system/start.sh
+++ test_system/start.sh
@@ -5,7 +5,7 @@
 # Get this file directory
 DIR=$(dirname "${BASH_SOURCE[0]}")
 
-echo "Starting services... ${@}"
+git submodule update --init
 
 # Check for .env file
 if [ ! -f "${DIR}/.env" ]; then
@@ -15,5 +15,7 @@ fi
 
 # Implement the docker-compose.yml file
 #cp "${DIR}/orchestrator-init "${DIR}/wasmiot-orchestrator/init"
+
+echo "Starting services... ${@}"
 
 docker compose -f "${DIR}/docker-compose.yml" --profile device up --pull always ${@}
